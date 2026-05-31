05b85f180f0c14b4c1dbfbc0adb475db208b64bd
Santiago Sepúlveda Rojas <santiago.sepulveda@DATALYTICS.local>
2026-05-30 19:19:07 -0500

    avance ENTREGA 2.1

--- myanswers/answer-0330.py
+++ myanswers/answer-0330.py
@@ -1,4 +1,3 @@
-import numpy as np
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_validate
@@ -8,18 +7,15 @@ def select_and_evaluate(df, target_col, k=3):
     X = df.drop(columns=[target_col])
     y = df[target_col]
     
-    # Seleccionar las k mejores características
     selector = SelectKBest(f_regression, k=k)
     selector.fit(X, y)
     best_features = list(X.columns[selector.get_support()])
     
-    # Pipeline con Ridge
     pipeline = Pipeline([
         ('selector', SelectKBest(f_regression, k=k)),
         ('model', Ridge())
     ])
     
-    # Validación cruzada 5 folds
     cv_results = cross_validate(pipeline, X, y, cv=5,
                                 scoring=['neg_root_mean_squared_error', 'r2'])
     
